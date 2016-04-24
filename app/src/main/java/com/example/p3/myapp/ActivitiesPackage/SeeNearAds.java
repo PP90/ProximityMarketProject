ba1d7537e4ae8857788f2a0a311edc5dd6b7b7cd
PP90 <pietropiscione@gmail.com>
2016-04-24 15:31:59 +0200

    All missed activities have been added from FaGr source code. The merging process is completed.

---
+++ app/src/main/java/com/example/p3/myapp/ActivitiesPackage/SeeNearAds.java
@@ -0,0 +1,15 @@
+package com.example.p3.myapp.ActivitiesPackage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.p3.myapp.R;
+
+public class SeeNearAds extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_see_near_ads);
+    }
+}
--- app/src/main/java/com/example/p3/myapp/ActivitiesPackage/UserActivity.java
+++ app/src/main/java/com/example/p3/myapp/ActivitiesPackage/UserActivity.java
@@ -1,15 +1,52 @@
 package com.example.p3.myapp.ActivitiesPackage;
 
+import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.p3.myapp.R;
 
-public class UserActivity extends AppCompatActivity {
+public class UserActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+        Button addNewAd=(Button) findViewById(R.id.button_add_ads);
+        addNewAd.setOnClickListener(this);
+        Button seeNearAds=(Button) findViewById(R.id.button_see_near_ads);
+        seeNearAds.setOnClickListener(this);
+        Button seeMyAds=(Button) findViewById(R.id.button_see_my_ads);
+        seeMyAds.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button_add_ads:
+                Intent goToAddNewAddActivity=new Intent(this, InsertAd.class);
+                Log.i("UserActivity", "Add new ad button pressed");
+                startActivity(goToAddNewAddActivity);
+                break;
+
+            case R.id.button_see_near_ads:
+                Intent goToSeeNearAdActivity=new Intent(this, SeeNearAds.class);
+                Log.i("UserActivity", "See near ad button pressed");
+                startActivity(goToSeeNearAdActivity);
+                break;
+
+            case R.id.button_see_my_ads:
+                Intent goToSeeMyAdActivity=new Intent(this, MyAds.class);
+                Log.i("UserActivity", "My ads button pressed");
+                startActivity(goToSeeMyAdActivity);
+                break;
+
+            default:
+                break;
+        }
     }
 }
